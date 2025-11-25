ebe0e82cf5c7c80622ca663ae5358a9f8d0751f7
NobleWolf412 <maccardi4431@gmail.com>
2025-11-25 01:01:18 +0000

    Edited Spark

--- src/hooks/useMousePosition.ts
+++ src/hooks/useMousePosition.ts
@@ -3,10 +3,10 @@ import { useEffect, useState } from 'react';
 export function useMousePosition() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      setPosition({ x: e.clientX, y: e.clientY });
+    window.addEvent
+    return () => {
     };
+
 
     window.addEventListener('mousemove', handleMouseMove);
 
