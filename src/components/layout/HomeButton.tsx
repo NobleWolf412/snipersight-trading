194bab2af0e5943f2b1aa6992d99b1aedc7dcd28
NobleWolf412 <maccardi4431@gmail.com>
2025-11-25 00:45:55 +0000

    Edited Spark

--- src/components/layout/HomeButton.tsx
+++ src/components/layout/HomeButton.tsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
-import { Button } from '@/components/ui/button';
 import { House } from '@phosphor-icons/react';
+export function HomeButton() {
 
 export function HomeButton() {
   return (
     <Link to="/">
       <Button 
-        variant="outline" 
+        <House size={18} w
         size="sm"
         className="h-10 px-4 gap-2 hover:border-accent/50 hover:bg-accent/10 transition-all"
       >
@@ -14,5 +14,5 @@ export function HomeButton() {
         Home
       </Button>
     </Link>
-  );
-}
+
+
